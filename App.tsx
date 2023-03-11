e09787f2ca080bc6c548dd9b9ee0e7e113322c25
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-12 00:32:11 +0800

    feat(chatgpt): init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,49 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, View, Text, StyleSheet, AppState} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  AppState,
+  FlatList,
+} from 'react-native';
 import Clipboard from '@react-native-clipboard/clipboard';
 import OpenAIKeyInputBox from './OpenAIKeyInputBox';
 
+const questions = [
+  (sentence: string) => `${sentence}\n可以用表格的方式总结观点：`,
+  (sentence: string) => `${sentence}\n如果用现实生活中的例子来说，就是：`,
+  (sentence: string) => `${sentence}\n类似的句子还有：`,
+  (sentence: string) => `${sentence}\n相反的观点有：`,
+  (sentence: string) =>
+    `${sentence}\n不同生活环境，不同职业的人会有不同的视角，比如：`,
+  (sentence: string) => `${sentence}\n这句话相关历史和背景是：`,
+  (sentence: string) => `${sentence}\n不同的国家对这句的看法会是：`,
+  (sentence: string) => `${sentence}\n如果莎士比亚将它写成中文诗会是：`,
+  (sentence: string) =>
+    `${sentence}\n想要深入了解这句话，推荐以下的文章、书籍：`,
+];
+
+type one_conversation = {
+  prompt: string;
+  response: string;
+};
+
 const App = () => {
   const [copiedText, setCopiedText] = useState('');
+  const [conversations, setConversations] = useState<one_conversation[]>([]);
+  useEffect(() => {
+    if (copiedText !== '') {
+      const prompts = questions.map(q => q(copiedText));
+      setConversations(
+        prompts.map(prompt => ({
+          prompt,
+          response: '',
+        })),
+      );
+    }
+  }, [copiedText]);
+
   const checkClipboardContent = async () => {
     const content = await Clipboard.getString();
     if (content !== '') {
@@ -27,6 +66,16 @@ const App = () => {
       <View style={styles.container}>
         <Text style={styles.copiedText}>{copiedText}</Text>
       </View>
+      <FlatList
+        data={conversations}
+        renderItem={({item}) => (
+          <View style={styles.container}>
+            <Text>{item.prompt}</Text>
+            <Text>{item.response}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.prompt}
+      />
     </SafeAreaView>
   );
 };
