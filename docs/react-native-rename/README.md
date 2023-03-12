e6feb05cce0968ade070d669f0c331a575ad2205
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-03-12 19:02:04 +0800

    docs(repo): rename

---
+++ docs/react-native-rename/README.md
@@ -0,0 +1,5 @@
+https://github.com/junedomingo/react-native-rename
+
+```shell
+npx react-native-rename@latest "new_name"
+```
--- ios/AskMoreTests/AskMoreTests.m
+++ ios/AskMoreTests/AskMoreTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AutoCopyTests : XCTestCase
+@interface AskMoreTests : XCTestCase
 
 @end
 
-@implementation AutoCopyTests
+@implementation AskMoreTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
